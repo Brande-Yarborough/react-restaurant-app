8389a014e72f2b2cdae3d4e0858faaf316213aa1
BrandeYarborough <brandeyarborough@gmail.com>
2023-02-04 17:40:51 -0500

    completes carousel

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import "./App.css";
+import UncontrolledExample from "./components/ImageCarousel";
 
 function App() {
   const [order, setOrder] = useState([]);
@@ -15,27 +16,29 @@ function App() {
     //   </div>
     //   <MenuList />
     // </div>
-
-    <Navbar className="navbar" fixed="top">
-      <Container>
-        <Navbar.Brand href="#home">
-          <img
-            src="https://static.spotapps.co/website_images/ab_websites/105011_website/logo_scroll.png"
-            width="50"
-            height="50"
-            className="d-inline-block align-top"
-            alt="Farm Fresh Fast logo"
-          />
-        </Navbar.Brand>{" "}
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#about">About</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <div className="App">
+      <Navbar className="navbar" fixed="top">
+        <Container>
+          <Navbar.Brand href="#home">
+            <img
+              src="https://static.spotapps.co/website_images/ab_websites/105011_website/logo_scroll.png"
+              width="65"
+              height="65"
+              className="d-inline-block align-top"
+              alt="Farm Fresh Fast logo"
+            />
+          </Navbar.Brand>{" "}
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#about">About</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <UncontrolledExample />
+    </div>
   );
 }
 
---
+++ src/components/ImageCarousel.js
@@ -0,0 +1,27 @@
+import Carousel from "react-bootstrap/Carousel";
+
+function UncontrolledExample() {
+  return (
+    <Carousel>
+      <Carousel.Item>
+        <img className="d-block w-100" src={require("../assets/farmbuilding.jpeg")} fluid alt="First slide" />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={require("../assets/farmfreshfastheroimage 2.jpg")}
+          alt="Second slide"
+        />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={require("../assets/farmfreshfastheroimage 3.jpeg")}
+          alt="Third slide"
+        />
+      </Carousel.Item>
+    </Carousel>
+  );
+}
+
+export default UncontrolledExample;
