fb98abe577bf8ccae40273973c66c243d194eccc
BrandeYarborough <brandeyarborough@gmail.com>
2023-02-15 14:12:50 -0500

    updates handle submit for local storage to work properly

--- src/components/MenuList.js
+++ src/components/MenuList.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { nanoid } from "nanoid";
 import MenuItem from "./MenuItem";
-import Nav from 'react-bootstrap/Nav';
+import Nav from "react-bootstrap/Nav";
 
 const INITIAL_MENU_ITEMS = [
   {
@@ -18,87 +18,97 @@ const INITIAL_MENU_ITEMS = [
     description:
       "Hormone-free chicken, turkey bacon, farmhouse blended cheese, onions, and greens in a spinach wrap, toasted. | Choice of side. | 555 cal",
     price: 12.99,
-    category: "Farm To Hands",  },
+    category: "Farm To Hands",
+  },
   {
-    id: 3, 
+    id: 3,
     name: "Buffalo Chicken Quesadilla",
-    description: "Hormone-free chicken, buffalo sauce, onions, bleu cheese, and blended cheese, grilled in a spinach wrap. | 490 cal",
+    description:
+      "Hormone-free chicken, buffalo sauce, onions, bleu cheese, and blended cheese, grilled in a spinach wrap. | 490 cal",
     price: 11.99,
     category: "Farm To Hands",
   },
   {
-    id: 4, 
+    id: 4,
     name: "Seasonal Stir Fry",
-    description: "Brown rice, squash/zucchini, peppers, onions, and house-made stir fry sauce. | 455 cal",
+    description:
+      "Brown rice, squash/zucchini, peppers, onions, and house-made stir fry sauce. | 455 cal",
     price: 10.99,
     category: "Farm Favorites",
   },
   {
-    id: 5, 
+    id: 5,
     name: "Farmhouse Skillet",
-    description: "Sauteed onions, peppers, collard greens, sweet potatoes, and mushrooms, topped with hormone-free chicken and an over-easy chicken egg*. | 450 cal",
+    description:
+      "Sauteed onions, peppers, collard greens, sweet potatoes, and mushrooms, topped with hormone-free chicken and an over-easy chicken egg*. | 450 cal",
     price: 14.99,
     category: "Farm Favorites",
   },
   {
-    id: 6, 
+    id: 6,
     name: "Coal Miner's Daughter Seared Meatloaf",
-    description: "Locally sourced grass-fed beef. Onions and peppers, topped with house-made mountaineer sauce. | Choice of two sides. | 645 cal",
+    description:
+      "Locally sourced grass-fed beef. Onions and peppers, topped with house-made mountaineer sauce. | Choice of two sides. | 645 cal",
     price: 15.99,
     category: "Farm Favorites",
   },
   {
-    id: 7, 
+    id: 7,
     name: "Farmhouse Salad - GF",
-    description: "Field greens, cucumbers, seasonal veggies, and blended cheese, tossed. | House dressing on the side. | 180 cal",
+    description:
+      "Field greens, cucumbers, seasonal veggies, and blended cheese, tossed. | House dressing on the side. | 180 cal",
     price: 9.99,
     category: "Salads",
   },
   {
-    id: 8, 
+    id: 8,
     name: "Seasonal Cobb Salad - GF",
-    description: "Mixed greens, cucumbers, micro greens, blended cheese, fried egg, and turkey bacon. | House dressing on the side. | 315 cal",
+    description:
+      "Mixed greens, cucumbers, micro greens, blended cheese, fried egg, and turkey bacon. | House dressing on the side. | 315 cal",
     price: 11.99,
     category: "Salads",
   },
   {
-    id: 9, 
+    id: 9,
     name: "Apple Harvest Salad - GF",
-    description: "Mixed greens, cucumbers, carrots, onion, sliced apples, and feta cheese, topped with chicken. | House dressing on the side. | 525 cal",
+    description:
+      "Mixed greens, cucumbers, carrots, onion, sliced apples, and feta cheese, topped with chicken. | House dressing on the side. | 525 cal",
     price: 13.99,
     category: "Salads",
   },
   {
-    id: 10, 
+    id: 10,
     name: "Vegan Chicken Bacon Ranch Wrap",
-    description: "Gardein no-kill vegan chicken, sweet potato bacon, onions, and vegan cheese in a spinach wrap, toasted. | Served with house-made vegan ranch on the side.",
+    description:
+      "Gardein no-kill vegan chicken, sweet potato bacon, onions, and vegan cheese in a spinach wrap, toasted. | Served with house-made vegan ranch on the side.",
     price: 13.99,
     category: "No-Kill Menu",
   },
   {
-    id: 11, 
+    id: 11,
     name: "Vegan Buffalo Chicken Wrap",
-    description: "Gardein no-kill vegan chicken tossed in buffalosauce, vegan cheese, cucumbers, and mixed greens in a spinach wrap, toasted. | Served with house-made vegan ranch on the side.",
+    description:
+      "Gardein no-kill vegan chicken tossed in buffalosauce, vegan cheese, cucumbers, and mixed greens in a spinach wrap, toasted. | Served with house-made vegan ranch on the side.",
     price: 13.99,
     category: "No-Kill Menu",
   },
   {
-    id: 12, 
+    id: 12,
     name: "Backyard Bacon Cheeseburger",
-    description: "Beyond meat beef, tempeh bacon, vegan cheese, topped with lettuce, green tomato, pickles, onion",
+    description:
+      "Beyond meat beef, tempeh bacon, vegan cheese, topped with lettuce, green tomato, pickles, onion",
     price: 14.99,
     category: "No-Kill Menu",
-  }
+  },
 ];
 
-function MenuList({addToCart}) {
+function MenuList({ addToCart }) {
   const [menuItems, setMenuItems] = useState(INITIAL_MENU_ITEMS);
   const [filter, setFilter] = useState("Farm to Hands");
   const [name, setName] = useState("");
   const [price, setPrice] = useState("");
   const [description, setDescription] = useState("");
 
-  
   // const innerHTML = INITIAL_MENU_ITEMS.map((x) => (
   //   <div key={x.id}>
   //   <div>{x.name}</div>
@@ -110,35 +120,62 @@ function MenuList({addToCart}) {
   // )
 
   const menuItemsHTML = menuItems
-  .filter((menuItem) => menuItem.category.toLowerCase() === filter.toLowerCase())
-  .map((menuItem) => (
-    <MenuItem key={menuItem.id} name={menuItem.name} description={menuItem.description} price={menuItem.price} addToCart={addToCart}/>
-  ));
+    .filter(
+      (menuItem) => menuItem.category.toLowerCase() === filter.toLowerCase()
+    )
+    .map((menuItem) => (
+      <MenuItem
+        key={menuItem.id}
+        name={menuItem.name}
+        description={menuItem.description}
+        price={menuItem.price}
+        addToCart={addToCart}
+      />
+    ));
 
   return (
     <>
-    <Nav variant="tabs" defaultActiveKey="#first">
-      <Nav.Item>
-        <Nav.Link key="Farm to Hands" onClick={() => setFilter("Farm to Hands")} href="#first">Farm to Hands</Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link key="Farm Favorites" onClick={() => setFilter("Farm Favorites")} href="#favorites">Farm Favorites</Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link key="Salads" onClick={() => setFilter("Salads")} href="#salads">Salads</Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link key="No-Kill Menu" onClick={() => setFilter("No-Kill Menu")} href="#no-kill">No-Kill Menu</Nav.Link>
-      </Nav.Item>
-    </Nav>
-    <div>
-      {menuItemsHTML}
-    </div>
+      <Nav variant="tabs" defaultActiveKey="#first">
+        <Nav.Item>
+          <Nav.Link
+            key="Farm to Hands"
+            onClick={() => setFilter("Farm to Hands")}
+            href="#first"
+          >
+            Farm to Hands
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link
+            key="Farm Favorites"
+            onClick={() => setFilter("Farm Favorites")}
+            href="#favorites"
+          >
+            Farm Favorites
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link
+            key="Salads"
+            onClick={() => setFilter("Salads")}
+            href="#salads"
+          >
+            Salads
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link
+            key="No-Kill Menu"
+            onClick={() => setFilter("No-Kill Menu")}
+            href="#no-kill"
+          >
+            No-Kill Menu
+          </Nav.Link>
+        </Nav.Item>
+      </Nav>
+      <div>{menuItemsHTML}</div>
     </>
   );
-
-  
-  
 }
 
 export default MenuList;
